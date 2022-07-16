31e5da826e15687233e0d775d4524f4a4aae8bbd
David M <davidmanz@protonmail.com>
2022-07-16 23:02:42 +0200

    Add R3F

---
+++ src/Viewer.js
@@ -0,0 +1,39 @@
+import * as THREE from 'three'
+import * as React from 'react'
+import { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+
+function Box(props) {
+  // This reference will give us direct access to the THREE.Mesh object
+  const ref = useRef(null)
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+export default function Viewer() {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  )
+}
