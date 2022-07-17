469ce8ddc79e3c35ee15d488d4a0d83822799f7f
David M <davidmanz@protonmail.com>
2022-07-17 13:03:16 +0200

    animated electrical line

--- src/Viewer.js
+++ src/Viewer.js
@@ -1,7 +1,50 @@
 import * as THREE from 'three'
 import * as React from 'react'
-import { Canvas } from '@react-three/fiber'
-import { Line } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Line, Point, Points } from '@react-three/drei'
+import { useRef } from 'react'
+
+function ElectricalLine(props){
+  // #,#faa476,#f0746e,#e34f6f,#dc3977,#b9257a,#7c1d6f
+  const color1 = new THREE.Color( 0xfcde9c );
+  const color2 = new THREE.Color( 0x7c1d6f );
+  const color = color1.lerp(color2, props.voltage);
+  const src = new THREE.Vector3(...props.src);
+  const dst = new THREE.Vector3(...props.dst);
+  const diff = dst.clone().sub(src);
+  const dir = diff.clone().normalize();
+  const normal = diff.clone().cross(new THREE.Vector3(0, 0, 1)).normalize();
+
+  const density = 6;
+  const numPoints = Math.floor(diff.length()*density);
+  const pointRefs = [...Array(numPoints).keys()].map(useRef);
+  const electricPoints = pointRefs.map((ref, x) =>
+    (<Point position={src.clone().add(dir.clone().multiplyScalar(x/density))} key={x} ref={ref} x={x/numPoints}/>)
+  );
+  useFrame((state, delta) => {
+    pointRefs.forEach(ref => {
+      ref.current.x += 0.4*delta;
+      ref.current.x -= Math.floor(ref.current.x);
+      ref.current.position.lerpVectors(src, dst, ref.current.x);
+    });
+  });
+  return (
+    <group>
+      <Line
+      points={[props.src, props.dst]}
+      color="white"
+      lineWidth={1}
+      />
+      <Points
+        limit={1000} // Optional: max amount of items (for calculating buffer size)
+        range={1000} // Optional: draw-range
+      >
+        <pointsMaterial  color={color} size={0.06}/>
+        {electricPoints}
+      </Points>
+    </group>
+  )
+}
 
 class Cable extends React.Component{
   render() {
@@ -10,7 +53,6 @@ class Cable extends React.Component{
         points={[this.props.src, this.props.dst]}
         color="gray"
         lineWidth={1}
-        dashed={false}
         />
     )
   }
@@ -103,7 +145,7 @@ export default function Viewer() {
   return (
     <Canvas>
       <Battery negative={[-0.5,3,0]} positive={[0.5,3,0]}></Battery>
-      <Cable src={[0.5, 3, 0]} dst={[2,3,0]}></Cable>
+      <ElectricalLine src={[0.5, 3, 0]} dst={[2,3,0]} voltage={1} current={0.5}></ElectricalLine>
       <Cable src={[2, 3, 0]} dst={[2,0,0]}></Cable>
       <Resistor src={[-2,-1,0]} dst={[2, -1, 0]}></Resistor>
     </Canvas>
