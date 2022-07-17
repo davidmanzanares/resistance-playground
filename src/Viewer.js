21efaa93a09842e994e6099874de313fcefe89ec
David M <davidmanz@protonmail.com>
2022-07-17 15:54:12 +0200

    improve electrical line animation

--- src/Viewer.js
+++ src/Viewer.js
@@ -5,36 +5,55 @@ import { Line, Point, Points } from '@react-three/drei'
 import { useRef } from 'react'
 
 function ElectricalLine(props){
-  // #,#faa476,#f0746e,#e34f6f,#dc3977,#b9257a,#7c1d6f
   const color1 = new THREE.Color( 0xfcde9c );
   const color2 = new THREE.Color( 0x7c1d6f );
   const color = color1.lerp(color2, props.voltage);
-  const src = new THREE.Vector3(...props.src);
-  const dst = new THREE.Vector3(...props.dst);
-  const diff = dst.clone().sub(src);
-  const dir = diff.clone().normalize();
-  const normal = diff.clone().cross(new THREE.Vector3(0, 0, 1)).normalize();
+
+  const directions = [];
+  const segmentLengths = [];
+  const points = props.points.map(p => new THREE.Vector3(...p));
+  const lines = [];
+  points.slice(0, points.length-1).forEach((src, i) => {
+    const dst = points[i+1];
+    const diff = dst.clone().sub(src);
+    segmentLengths.push(diff.length());
+    directions.push(diff.normalize());
+    lines.push((<Line
+      points={[src, dst]}
+      color="white"
+      lineWidth={1}
+      key={i}
+      />));
+  });
+  const lineLength = segmentLengths.reduce((x,y)=>x+y);
+
+  function vToPos(v) {
+    for (let i=0; i<segmentLengths.length; i++){
+      if (v<segmentLengths[i]){
+        return points[i].clone().add(directions[i].clone().multiplyScalar(v));
+      }
+      v-= segmentLengths[i];
+    }
+    return new THREE.Vector3();
+  }
 
   const density = 6;
-  const numPoints = Math.floor(diff.length()*density);
+  const numPoints = Math.round(lineLength*density);
   const pointRefs = [...Array(numPoints).keys()].map(useRef);
-  const electricPoints = pointRefs.map((ref, x) =>
-    (<Point position={src.clone().add(dir.clone().multiplyScalar(x/density))} key={x} ref={ref} x={x/numPoints}/>)
+  const electricPoints = pointRefs.map((ref, i) => 
+    (<Point position={vToPos(i/density)} key={i} ref={ref} v={i/density}/>)
   );
+
   useFrame((state, delta) => {
     pointRefs.forEach(ref => {
-      ref.current.x += 0.4*delta;
-      ref.current.x -= Math.floor(ref.current.x);
-      ref.current.position.lerpVectors(src, dst, ref.current.x);
+      ref.current.v = (1.*delta + ref.current.v) % lineLength;
+      const p = vToPos(ref.current.v);
+      ref.current.position.set(p.x, p.y, p.z);
     });
   });
   return (
     <group>
-      <Line
-      points={[props.src, props.dst]}
-      color="white"
-      lineWidth={1}
-      />
+      {lines}
       <Points
         limit={1000} // Optional: max amount of items (for calculating buffer size)
         range={1000} // Optional: draw-range
@@ -46,27 +65,16 @@ function ElectricalLine(props){
   )
 }
 
-class Cable extends React.Component{
-  render() {
-    return (
-      <Line
-        points={[this.props.src, this.props.dst]}
-        color="gray"
-        lineWidth={1}
-        />
-    )
-  }
-}
-
 class Battery extends React.Component{
   render(){
     const negative = new THREE.Vector3(...this.props.negative);
     const positive = new THREE.Vector3(...this.props.positive);
     const center = negative.clone().add(positive).multiplyScalar(0.5);
-    const negativeToPositive = positive.clone().sub(negative);
-    const negativeMid = center.clone().sub(negativeToPositive.multiplyScalar(0.2));
-    const positiveMid = center.clone().add(negativeToPositive.multiplyScalar(0.2));
-    const normal = negativeToPositive.clone().cross(new THREE.Vector3(0, 0, 1)).normalize();
+    const diff = positive.clone().sub(negative);
+    const dir = diff.clone().normalize();
+    const negativeMid = center.clone().sub(dir.clone().multiplyScalar(0.05));
+    const positiveMid = center.clone().add(dir.clone().multiplyScalar(0.1));
+    const normal = diff.clone().cross(new THREE.Vector3(0, 0, 1)).normalize();
     const negativePerpendicularA = negativeMid.clone().add(normal.clone().multiplyScalar(0.25));
     const negativePerpendicularB = negativeMid.clone().sub(normal.clone().multiplyScalar(0.25));
     const positivePerpendicularA = positiveMid.clone().add(normal.clone().multiplyScalar(0.5));
@@ -75,22 +83,22 @@ class Battery extends React.Component{
       <group>
       <Line
         points={[negative, negativeMid]}
-        color="blue"
+        color="#fcde9c"
         lineWidth={1}
         />
       <Line
         points={[negativePerpendicularA, negativePerpendicularB]}
-        color="blue"
+        color="#fcde9c"
         lineWidth={1}
         />
       <Line
         points={[positiveMid, positive]}
-        color="red"
+        color="#7c1d6f"
         lineWidth={1}
         />
       <Line
         points={[positivePerpendicularA, positivePerpendicularB]}
-        color="red"
+        color="#7c1d6f"
         lineWidth={1}
         />
       </group>
@@ -144,9 +152,8 @@ class Resistor extends React.Component{
 export default function Viewer() {
   return (
     <Canvas>
-      <Battery negative={[-0.5,3,0]} positive={[0.5,3,0]}></Battery>
-      <ElectricalLine src={[0.5, 3, 0]} dst={[2,3,0]} voltage={1} current={0.5}></ElectricalLine>
-      <Cable src={[2, 3, 0]} dst={[2,0,0]}></Cable>
+      <Battery negative={[-0.4,3,0]} positive={[0.1,3,0]}></Battery>
+      <ElectricalLine points={[[0.1, 3, 0], [2,3,0], [2,0,0]]} voltage={1} current={0.5}></ElectricalLine>
       <Resistor src={[-2,-1,0]} dst={[2, -1, 0]}></Resistor>
     </Canvas>
   )
